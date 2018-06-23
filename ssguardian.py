79b1598d6e60ff0f412240eca6ad8dae43432626
老佘百分百 <sheshuaiyan@163.com>
2018-06-23 12:07:04 +0800

    Create ssguardian.py

---
+++ ssguardian.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+import os
+import time
+import datetime
+while True:
+    a = os.popen('ps -ef|grep ssserver|grep -v grep').read()#获取执行命令的结果，OS.SYSTEM是执行并打印执行结果，返回值是其他东西
+    print a + str(datetime.datetime.now())
+    if 'ssserver' in a:
+        print 'do nothing' + str(datetime.datetime.now())
+    else:
+        os.system('ssserver -p 8000 -k password -m rc4-md5 -d start')
+    time.sleep(1000)
